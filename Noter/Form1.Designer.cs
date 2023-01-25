1704a009c5aa8d6de761f64149e96d5114be3d29
GeorgiKatsarov <100018528+GeorgiKatsarov@users.noreply.github.com>
2023-01-25 17:16:17 +0200

    An app with more data structures

---
+++ Noter/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace Noter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OldNotes = new System.Windows.Forms.DataGridView();
+            this.TitleTextBox = new System.Windows.Forms.TextBox();
+            this.NoteTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.bttnSave = new System.Windows.Forms.Button();
+            this.bttnClose = new System.Windows.Forms.Button();
+            this.bttnNew = new System.Windows.Forms.Button();
+            this.bttnDelete = new System.Windows.Forms.Button();
+            this.bttnLoad = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.OldNotes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // OldNotes
+            // 
+            this.OldNotes.AllowUserToAddRows = false;
+            this.OldNotes.AllowUserToDeleteRows = false;
+            this.OldNotes.AllowUserToResizeColumns = false;
+            this.OldNotes.AllowUserToResizeRows = false;
+            this.OldNotes.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.OldNotes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.OldNotes.Location = new System.Drawing.Point(41, 36);
+            this.OldNotes.Name = "OldNotes";
+            this.OldNotes.RowHeadersWidth = 62;
+            this.OldNotes.RowTemplate.Height = 28;
+            this.OldNotes.Size = new System.Drawing.Size(390, 461);
+            this.OldNotes.TabIndex = 0;
+            // 
+            // TitleTextBox
+            // 
+            this.TitleTextBox.Location = new System.Drawing.Point(482, 83);
+            this.TitleTextBox.Name = "TitleTextBox";
+            this.TitleTextBox.Size = new System.Drawing.Size(465, 26);
+            this.TitleTextBox.TabIndex = 1;
+            // 
+            // NoteTextBox
+            // 
+            this.NoteTextBox.Location = new System.Drawing.Point(482, 150);
+            this.NoteTextBox.Multiline = true;
+            this.NoteTextBox.Name = "NoteTextBox";
+            this.NoteTextBox.Size = new System.Drawing.Size(465, 503);
+            this.NoteTextBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(478, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Title";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Location = new System.Drawing.Point(478, 116);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Note";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // bttnSave
+            // 
+            this.bttnSave.Location = new System.Drawing.Point(41, 515);
+            this.bttnSave.Name = "bttnSave";
+            this.bttnSave.Size = new System.Drawing.Size(115, 57);
+            this.bttnSave.TabIndex = 6;
+            this.bttnSave.Text = "Save";
+            this.bttnSave.UseVisualStyleBackColor = true;
+            this.bttnSave.Click += new System.EventHandler(this.bttnSave_Click);
+            // 
+            // bttnClose
+            // 
+            this.bttnClose.Location = new System.Drawing.Point(175, 587);
+            this.bttnClose.Name = "bttnClose";
+            this.bttnClose.Size = new System.Drawing.Size(115, 57);
+            this.bttnClose.TabIndex = 7;
+            this.bttnClose.Text = "Close";
+            this.bttnClose.UseVisualStyleBackColor = true;
+            this.bttnClose.Click += new System.EventHandler(this.bttnClose_Click);
+            // 
+            // bttnNew
+            // 
+            this.bttnNew.Location = new System.Drawing.Point(41, 587);
+            this.bttnNew.Name = "bttnNew";
+            this.bttnNew.Size = new System.Drawing.Size(115, 57);
+            this.bttnNew.TabIndex = 8;
+            this.bttnNew.Text = "New";
+            this.bttnNew.UseVisualStyleBackColor = true;
+            this.bttnNew.Click += new System.EventHandler(this.bttnNew_Click);
+            // 
+            // bttnDelete
+            // 
+            this.bttnDelete.Location = new System.Drawing.Point(316, 515);
+            this.bttnDelete.Name = "bttnDelete";
+            this.bttnDelete.Size = new System.Drawing.Size(115, 57);
+            this.bttnDelete.TabIndex = 9;
+            this.bttnDelete.Text = "Delete";
+            this.bttnDelete.UseVisualStyleBackColor = true;
+            this.bttnDelete.Click += new System.EventHandler(this.bttnDelete_Click);
+            // 
+            // bttnLoad
+            // 
+            this.bttnLoad.Location = new System.Drawing.Point(175, 515);
+            this.bttnLoad.Name = "bttnLoad";
+            this.bttnLoad.Size = new System.Drawing.Size(115, 57);
+            this.bttnLoad.TabIndex = 10;
+            this.bttnLoad.Text = "Load";
+            this.bttnLoad.UseVisualStyleBackColor = true;
+            this.bttnLoad.Click += new System.EventHandler(this.bttnLoad_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(974, 674);
+            this.Controls.Add(this.bttnLoad);
+            this.Controls.Add(this.bttnDelete);
+            this.Controls.Add(this.bttnNew);
+            this.Controls.Add(this.bttnClose);
+            this.Controls.Add(this.bttnSave);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.NoteTextBox);
+            this.Controls.Add(this.TitleTextBox);
+            this.Controls.Add(this.OldNotes);
+            this.Name = "Form1";
+            this.Text = "Noter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.OldNotes)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView OldNotes;
+        private System.Windows.Forms.TextBox TitleTextBox;
+        private System.Windows.Forms.TextBox NoteTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button bttnSave;
+        private System.Windows.Forms.Button bttnClose;
+        private System.Windows.Forms.Button bttnNew;
+        private System.Windows.Forms.Button bttnDelete;
+        private System.Windows.Forms.Button bttnLoad;
+    }
+}
+
---
+++ Noter/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Noter
+{
+    public partial class Form1 : Form
+    {
+        DataTable notes = new DataTable();
+        bool editing = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+          
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            notes.Columns.Add("Title");
+            notes.Columns.Add("Note");
+            OldNotes.DataSource = notes;
+        }
+
+        private void bttnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void bttnSave_Click(object sender, EventArgs e)
+        {
+            if (editing)
+            {
+                notes.Rows[OldNotes.CurrentCell.RowIndex]["Title"] = TitleTextBox.Text;
+                notes.Rows[OldNotes.CurrentCell.RowIndex]["Note"] = NoteTextBox.Text;
+            }
+            else
+            {
+                notes.Rows.Add(TitleTextBox.Text, NoteTextBox.Text);
+
+            }
+            editing = false;
+            TitleTextBox.Text = "";
+            NoteTextBox.Text = "";
+        }
+
+        private void bttnLoad_Click(object sender, EventArgs e)
+        {
+            TitleTextBox.Text = notes.Rows[OldNotes.CurrentCell.RowIndex].ItemArray[0].ToString();
+            NoteTextBox.Text = notes.Rows[OldNotes.CurrentCell.RowIndex].ItemArray[1].ToString();
+            editing = true;
+        }
+
+        private void bttnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                notes.Rows[OldNotes.CurrentCell.RowIndex].Delete();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Something failed");
+            }
+        }
+
+        private void bttnNew_Click(object sender, EventArgs e)
+        {
+            TitleTextBox.Text = "";
+            NoteTextBox.Text = "";
+        }
+    }
+}
